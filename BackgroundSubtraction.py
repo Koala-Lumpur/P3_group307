e428cfba1300454f565f7e27a3114a928090db8f
Rasmus Nielsen <rewn97@gmail.com>
2018-10-09 13:22:14 +0200

    Basic background subtraction
    Two different methods are used for background subtration at this point, we might want to look into eac one

--- BackgroundSubtraction.py
+++ BackgroundSubtraction.py
@@ -4,6 +4,12 @@ import cv2
 # Get the capture device
 video = cv2.VideoCapture(0);
 
+ret, firstframe = video.read()
+firstframe = cv2.flip(firstframe, 1)
+
+# Define the mask to be used for background subtraction
+sub = cv2.createBackgroundSubtractorMOG2(history=20, varThreshold=16)
+
 while (True):
     # Capture video
     ret, frame = video.read()
@@ -11,8 +17,15 @@ while (True):
     # Flip video frame by frame
     frame = cv2.flip(frame, 1)
 
+    # Apply mask to video capture
+    bgmask = sub.apply(frame)
+
+    diff = cv2.absdiff(firstframe, frame)
+
     # Show the video
     cv2.imshow('frame', frame)
+    cv2.imshow('Background sub', bgmask)
+    cv2.imshow('Difference', diff)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
--- EdgeDetection.py
+++ EdgeDetection.py
@@ -1,19 +1,19 @@
 import numpy as np
 import cv2
 
-#Get the capture device
+# Get the capture device
 video = cv2.VideoCapture(0);
 
 while(True):
-    #Capture video
+    # Capture video
     ret, frame = video.read()
 
-    #Flip video frame by frame
+    # Flip video frame by frame
     frame = cv2.flip(frame, 1)
 
     canny = cv2.Canny(frame, 100, 200)
 
-    #Show the video
+    # Show the video
     cv2.imshow('Frame', frame)
     cv2.imshow('Canny', canny)
     if cv2.waitKey(1) & 0xFF == ord('q'):
