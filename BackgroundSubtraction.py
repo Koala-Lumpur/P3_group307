5fda8d8835221984c47ea9251390ca818d30e65d
Rasmus Nielsen <rewn97@gmail.com>
2018-10-09 10:29:01 +0200

    Simple canny edge detection
    Uses the Canny edge detection algorithm to find edges in the video from your video capture device. Also flips the video for better user experience

---
+++ BackgroundSubtraction.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+
+# Get the capture device
+video = cv2.VideoCapture(0);
+
+while (True):
+    # Capture video
+    ret, frame = video.read()
+
+    # Flip video frame by frame
+    frame = cv2.flip(frame, 1)
+
+    # Show the video
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
---
+++ EdgeDetection.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+
+#Get the capture device
+video = cv2.VideoCapture(0);
+
+while(True):
+    #Capture video
+    ret, frame = video.read()
+
+    #Flip video frame by frame
+    frame = cv2.flip(frame, 1)
+
+    canny = cv2.Canny(frame, 100, 200)
+
+    #Show the video
+    cv2.imshow('Frame', frame)
+    cv2.imshow('Canny', canny)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
