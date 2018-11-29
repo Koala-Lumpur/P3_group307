4a58e3f3f856c694b6a6789d3c318fb1e9c45977
Rasmus Nielsen <rewn97@gmail.com>
2018-11-29 14:59:50 +0100

    Implemented line and collision with that line

--- pygame_example.py
+++ pygame_example.py
@@ -17,17 +17,21 @@ font = pygame.font.Font(None, 30)
 clock = pygame.time.Clock()
 last_frame = pygame.time.get_ticks()
 speed_multiplier = 1
+line_x = 197
+line_y = 930
+line_w = 1531
+line_h = 46
 
 pygame.init()
 
-
-
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((0, 0))
 pygame.display.set_caption('WHat is thiis')
 pygame.display.toggle_fullscreen()
 
-background = pygame.image.load("background.jpg")
-wall = pygame.image.load("wall.jpeg")
+background = pygame.image.load("background.jpg").convert()
+wall = pygame.image.load("wall.jpeg").convert()
+line = pygame.image.load("line.png").convert_alpha()
+wall_mask = pygame.mask.from_surface(wall)
 #wall = pygame.transform.scale(wall, (wall.get_width()/3, wall.get_height()/3))
 
 class my_thread(threading.Thread):
@@ -37,13 +41,13 @@ class my_thread(threading.Thread):
         self.name = name
     def run(self):
         print("Starting " + self.name)
+        '''
         update()
 
 
 def update():
     while True:
-        fps = font.render(str(int(clock.get_fps())), True, pygame.Color('white'))
-        screen.blit(fps, (50, 50))
+    '''
 
 
 update_thread = my_thread(1, "update_thread")
@@ -51,7 +55,7 @@ update_thread.start()
 
 
 while True:
-    clock.tick(20)
+    clock.tick(60)
     start = pygame.time.get_ticks()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -60,27 +64,43 @@ while True:
         if event.key == pygame.K_ESCAPE:
             break
     screen.blit(background, [0, 0])
+    screen.blit(line, [line_x, line_y])
+    fps = font.render(str(int(clock.get_fps())), True, pygame.Color('white'))
+    screen.blit(fps, (50, 50))
+
     delta_time = (start - last_frame) / 1000
     last_frame = start
-    if not walls[0].x > 2000:
-        screen.blit(wall, (walls[0].new_wall_pos_x, walls[0].new_wall_pos_y))
+    if not walls[0].x > 5000:
+        screen.blit(wall, (walls[0].new_wall_pos_x*1.5, walls[0].new_wall_pos_y))
         wall = pygame.transform.smoothscale(wall, (int(walls[0].x), int(walls[0].y)))
-        walls[0].new_wall_pos_x -= 8.8 * delta_time * speed_multiplier
-        walls[0].new_wall_pos_y -= 4.7 * delta_time * speed_multiplier
-        walls[0].x += 18.5 * delta_time * speed_multiplier
-        walls[0].y += 10 * delta_time * speed_multiplier
+        walls[0].new_wall_pos_x -= int(8.8 * delta_time * speed_multiplier)
+        walls[0].new_wall_pos_y -= int(4.7 * delta_time * speed_multiplier)
+        walls[0].x += int(18.5 * delta_time * speed_multiplier)
+        walls[0].y += int(10 * delta_time * speed_multiplier)
         speed_multiplier += 1
+        if walls[0].new_wall_pos_y + walls[0].y > line_y:
+            wall_rect = wall.get_rect(center=(0, 0))
+            line_rect = line.get_rect(center=(0, 0))
+            offset_x = wall_rect.x - line_rect.x
+            offset_y = wall_rect.y - line_rect.y
+            wall_mask = pygame.mask.from_surface(wall)
+            hit = mask_ed.overlap(wall_mask, (offset_x, offset_y))
+            print(hit)
+            if hit:
+                print("hit")
 
-    '''
-    frame = bs.main()
+    
+    frame = ed.main()
     frame = pygame.surfarray.make_surface(frame)
+    frame.convert_alpha()
     frame_x = frame.get_width()
     frame_y = frame.get_height()
-    frame.convert_alpha()
-    frame.set_alpha(128)
     frame.set_colorkey((0, 0, 0))
+    mask_ed = pygame.mask.from_surface(frame)
     screen.blit(frame, (SCREEN_WIDTH/2 - frame_x/2, SCREEN_HEIGHT/2))
-    '''
+
+
+
 
     pygame.display.update()
 
