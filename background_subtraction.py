4d0fde19a147aac17efccc34a352c30f8efeddf7
Rasmus Nielsen <rewn97@gmail.com>
2018-12-06 13:21:45 +0100

    Code into different functions, collision is broken, and an "intro screen" has been implemented

--- background_subtraction.py
+++ background_subtraction.py
@@ -1,51 +1,54 @@
 import numpy as np
 import cv2
 
-video = cv2.VideoCapture(0);
-_, back_frame = video.read()
-back_frame = np.rot90(back_frame)
 
-def main():
+class BackSub:
+
+    video = cv2.VideoCapture(0);
+    first = True
+    global back_frame
+
+    def cap_frame(self):
+        _, back_frame = BackSub.video.read()
+        back_frame = np.rot90(back_frame)
+        back_frame = cv2.cvtColor(back_frame, cv2.COLOR_BGR2GRAY)
+        return back_frame
+
+    def main(self):
+
+        if BackSub.first:
+            global back_frame
+            back_frame = BackSub.cap_frame(self)
+            BackSub.first = False
+
+        # Capture video
+        ret, frame = BackSub.video.read()
+
+        # Flip video frame by frame
+        frame = np.rot90(frame)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        mask = cv2.subtract(back_frame, frame)
+        ret, thresh1 = cv2.threshold(mask, 25, 255, cv2.THRESH_BINARY)
+        #thresh1 = cv2.cvtColor(thresh1, cv2.COLOR_BGR2GRAY)
+
+        return thresh1
 
     '''
-    for i in range(framecount):
-        _, back_frame[i] = video.read()
-        ref_image = cv2.add(ref_image, back_frame[i])
-        print(i)
+    while True:
+        cv2.imshow('meh', main())
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
     
-    
-    ref_image = ref_image / framecount
-    ref_image = cv2.flip(ref_image, 1)
+    video.release()
+    cv2.destroyAllWindows()
     '''
 
-    # Capture video
-    ret, frame = video.read()
-
-    # Flip video frame by frame
-    frame = np.rot90(frame)
-
-    mask = cv2.subtract(back_frame, frame)
-
-    ret, thresh1 = cv2.threshold(mask, 25, 255, cv2.THRESH_BINARY)
-    thresh1 = cv2.cvtColor(thresh1, cv2.COLOR_BGR2GRAY)
-
-    return thresh1
-
-'''
-while True:
-    cv2.imshow('meh', main())
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-video.release()
-cv2.destroyAllWindows()
-'''
-
-'''
-# Show the video
-cv2.imshow('frame', frame)
-cv2.imshow('first frame', back_frame)
-cv2.imshow('ref_image', ref_image)
-cv2.imshow('mask', mask)
-cv2.imshow('threshold', thresh1)
-'''
+    '''
+    # Show the video
+    cv2.imshow('frame', frame)
+    cv2.imshow('first frame', back_frame)
+    cv2.imshow('ref_image', ref_image)
+    cv2.imshow('mask', mask)
+    cv2.imshow('threshold', thresh1)
+    '''
--- pygame_example.py
+++ pygame_example.py
@@ -7,127 +7,110 @@ import time
 import threading
 import wall
 
+bs_class = bs.BackSub()
+space_held = False
+game_screen = 0
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
 BLACK = (0,0,0)
 WHITE = (255,255,255)
-# An array - inside the [] we call the wall constructor in the Wall class (x, y pos and x, y scale for each wall)
-# The for loop makes it called 11 times and adds it to the array
 walls = [wall.Wall() for i in range(10)]
-# Initializing pygame-s font, so we can display the FPS (necessary for displaying any text)
 pygame.font.init()
-# Defining a new font object (first constructor the font type, none = pygame default font, second is the font size = 30)
+initial_font = pygame.font.Font(None, 100)
+
 font = pygame.font.Font(None, 30)
-# Used for limiting and getting the FPS
 clock = pygame.time.Clock()
-# Returns the number (in miliseconds) since pygame.init was called (before it's initialized it is zero)
 last_frame = pygame.time.get_ticks()
 speed_multiplier = 1
 line_x = 197
 line_y = 930
 line_w = 1531
 line_h = 46
-# initalizes pygame so we can call pygame methods
+
 pygame.init()
-# (0, 0) means it has the same resolution as the screen
+
 screen = pygame.display.set_mode((0, 0))
-# Window label (the name of the window)
-pygame.display.set_caption('Obstacle Course simulator 2018')
-# Maybe this isn't needed
+pygame.display.set_caption('WHat is thiis')
 pygame.display.toggle_fullscreen()
-# Loading the background image -
-# .convert() creates a new copy of the surface with the pixel format changed (it boosts FPS)
+
 background = pygame.image.load("background.jpg").convert()
 wall = pygame.image.load("wall.jpeg").convert()
 line = pygame.image.load("line.png").convert_alpha()
-# mask.from_surface is a pygame method - sorts out all the transparent pixels
-# We can use collision that ignores transparent pixels
 wall_mask = pygame.mask.from_surface(wall)
 #wall = pygame.transform.scale(wall, (wall.get_width()/3, wall.get_height()/3))
 
-# This doesn't do anything at the moment
-class my_thread(threading.Thread):
-    def __init__(self, threadID, name):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-    def run(self):
-        print("Starting " + self.name)
-        '''
-        update()
+
+def start_game(text, cap_frame):
+        screen.fill(BLACK)
+        initial_text = initial_font.render(text, False, pygame.Color('white'))
+        initial_text_rect = initial_text.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
+        screen.blit(initial_text, initial_text_rect)
+        pygame.display.update()
+        if cap_frame:
+            bs_class.main()
 
 
-def update():
-    while True:
-    '''
+def sub(u, v):
+    return [u[i]-v[i] for i in range(len(u))]
 
 
-update_thread = my_thread(1, "update_thread")
-update_thread.start()
-
-
-# Main loop
 while True:
-    # Setting the max FPS
-    clock.tick(60)
-    # Returns the time in milliseconds since pygame was initialized
-    start = pygame.time.get_ticks()
-    # The event loop - An open loop that makes the game continuously run (it's a necessity)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             break
-    # Once the escape key is hit, pygame.QUIT is called - which closese the whole game
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_ESCAPE:
             break
-    # The blit function is called on the screen object
-    # (first parameter - what we want blited, second parameter - pixel pos)
-    # Blitting is putting something on the screen - in this case we are visualizing the background
-    # (0, 0) is the anchor point of an image
-    screen.blit(background, [0, 0])
-    screen.blit(line, [line_x, line_y])
-    # Converting the number returned from clock.get_fps()
-    fps = font.render(str(int(clock.get_fps())), True, pygame.Color('white'))
-    # Rendering the FPS text
-    screen.blit(fps, (50, 50))
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_SPACE and not space_held:
+            game_screen += 1
+            space_held = True
+    elif event.type == pygame.KEYUP:
+        if event.key == pygame.K_SPACE:
+            space_held = False
 
-    # Calculating delta time
-    delta_time = (start - last_frame) / 1000
-    # Changing last frame to the actual last frame, so we can calculate again in the next frame
-    last_frame = start
-    # if statement that makes the walls disappearing after they reach a certain position
-    if not walls[0].x > 5000:
-        screen.blit(wall, (walls[0].new_wall_pos_x*1.5, walls[0].new_wall_pos_y))
-        wall = pygame.transform.smoothscale(wall, (int(walls[0].x), int(walls[0].y)))
-        walls[0].new_wall_pos_x -= int(8.8 * delta_time * speed_multiplier)
-        walls[0].new_wall_pos_y -= int(4.7 * delta_time * speed_multiplier)
-        walls[0].x += int(18.5 * delta_time * speed_multiplier)
-        walls[0].y += int(10 * delta_time * speed_multiplier)
-        speed_multiplier += 1
-        if walls[0].new_wall_pos_y + walls[0].y > line_y:
-            wall_rect = wall.get_rect(center=(0, 0))
-            line_rect = line.get_rect(center=(0, 0))
-            offset_x = wall_rect.x - line_rect.x
-            offset_y = wall_rect.y - line_rect.y
-            wall_mask = pygame.mask.from_surface(wall)
-            hit = mask_ed.overlap(wall_mask, (offset_x, offset_y))
-            print(hit)
-            if hit:
-                print("hit")
+    if game_screen == 0:
+        start_game("Press space to capture the first frame", False)
+    elif game_screen == 1:
+        start_game("Press space to continue to the game", True)
+    else:
+        clock.tick(60)
+        start = pygame.time.get_ticks()
+        screen.blit(background, [0, 0])
+        screen.blit(line, [line_x, line_y])
 
-    
-    frame = ed.main()
-    frame = pygame.surfarray.make_surface(frame)
-    frame.convert_alpha()
-    frame_x = frame.get_width()
-    frame_y = frame.get_height()
-    frame.set_colorkey((0, 0, 0))
-    mask_ed = pygame.mask.from_surface(frame)
-    screen.blit(frame, (SCREEN_WIDTH/2 - frame_x/2, SCREEN_HEIGHT/2))
+        delta_time = (start - last_frame) / 1000
+        last_frame = start
+        if not walls[0].x > 5000:
+            screen.blit(wall, (walls[0].new_wall_pos_x * 1.5, walls[0].new_wall_pos_y))
+            wall = pygame.transform.smoothscale(wall, (int(walls[0].x), int(walls[0].y)))
+            walls[0].new_wall_pos_x -= int(8.8 * delta_time * speed_multiplier)
+            walls[0].new_wall_pos_y -= int(4.7 * delta_time * speed_multiplier)
+            walls[0].x += int(18.5 * delta_time * speed_multiplier)
+            walls[0].y += int(10 * delta_time * speed_multiplier)
+            speed_multiplier += 1
+            if walls[0].new_wall_pos_y + walls[0].y > line_y:
+                wall_rect = wall.get_rect(center=(0, 0))
+                frame_rect = frame.get_rect(center=(0, 0))
+                wall_mask = pygame.mask.from_surface(wall)
+                offset_x = wall_rect.x - frame_rect.x
+                offset_y = wall_rect.y - frame_rect.y
+                hit = mask_ed.overlap(wall_mask, (offset_x, offset_y))
+                print(hit)
+                if hit:
+                    print("hit")
 
+        frame = bs_class.main()
+        frame = pygame.surfarray.make_surface(frame)
+        frame.convert_alpha()
+        frame_x = frame.get_width()
+        frame_y = frame.get_height()
+        frame.set_colorkey((0, 0, 0))
+        mask_ed = pygame.mask.from_surface(frame)
+        screen.blit(frame, (SCREEN_WIDTH / 2 - frame_x / 2, SCREEN_HEIGHT / 2))
 
+        fps = font.render(str(int(clock.get_fps())), False, pygame.Color('white'))
+        screen.blit(fps, (50, 50))
+        pygame.display.update()
 
-
-    pygame.display.update()
-
-pygame.quit()
+#pygame.quit()
